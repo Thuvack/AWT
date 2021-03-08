051b6fb56565d1cf310c519c32d8da0ecd95bb7c
Thuvack <ndubaneds@gmail.com>
2021-03-08 15:56:44 +0200

    Added script to find sum of Active editors across selected wikis

---
+++ SQL/AWT-V1-Slice2_CombActEditors.sql
@@ -0,0 +1,24 @@
+-- This script aggregates active editors across selected wikis
+
+USE awt;
+
+SELECT
+    sum(wiki.active_editors)
+FROM
+    awt.wikis as wiki
+JOIN
+    awt.languages_wikis as langwiki
+ON
+    wiki.wiki_name = langwiki.wiki_name
+WHERE
+ langwiki.language_code IN ('zu', 'ar')               -- Where languages are user selection
+ AND
+wiki.updated_at = ( SELECT                            -- Filter by last date of update in table
+                       wiki.updated_at
+                    FROM 
+                        wikis
+                    ORDER BY
+                        wiki.updated_at DESC LIMIT 1
+                    )
+;
+
