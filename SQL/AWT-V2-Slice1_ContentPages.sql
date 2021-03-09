787044696681eb978bfdc4a6873d78c7f1c1749a
Thuvack <ndubaneds@gmail.com>
2021-03-09 09:02:48 +0200

    Corrected View 2 script to fetch last six date records instead of current/last date record

--- SQL/AWT-V2-Slice1_ContentPages.sql
+++ SQL/AWT-V2-Slice1_ContentPages.sql
@@ -14,17 +14,8 @@ JOIN
 ON
 	wiki.wiki_name = langwiki.wiki_name
 WHERE
-langwiki.language_code IN ('zu', 'ar')               -- Where languages are user selection
- AND
-wiki.updated_at = ( SELECT                            -- Filter by last date of update in table
-                       wiki.updated_at
-                    FROM 
-                        wikis
-                    ORDER BY
-                        wiki.updated_at DESC LIMIT 1
-                    )
+langwiki.language_code IN ('zu', 'ar')       -- Where languages are user selection
 ORDER BY
-	wiki.updated_at DESC
+	wiki.updated_at DESC                 -- Fetch last 6 records in the date table
 LIMIT 6;
 
--- How does SQL handle multiple inputs for the WHERE statement?
